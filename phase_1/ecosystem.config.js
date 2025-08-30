cce530e41c072d2deb217779ec7fe9a2955ff54b
Daniel Chong <danielgq.chong@gmail.com>
2025-08-30 13:36:41 +1200

    update docker to serve the frontend

--- phase_1/ecosystem.config.js
+++ phase_1/ecosystem.config.js
@@ -30,12 +30,26 @@ module.exports = {
       min_uptime: "10s",
     },
     {
+      name: "frontend",
+      script: "./frontend/node_modules/.bin/serve",
+      args: "-s ./frontend/dist -l 3002",
+      cwd: "/app",
+      env: {
+        NODE_ENV: "production",
+      },
+      instances: 1,
+      exec_mode: "fork",
+      restart_delay: 1000,
+      max_restarts: 10,
+      min_uptime: "10s",
+    },
+    {
       name: "toniq-api",
       script: "./Toniq/dist/index.js",
       cwd: "/app",
       env: {
         NODE_ENV: "production",
-        PORT: 3002,
+        PORT: 3003,
         CENTRAL_API_URL: "http://localhost:3000",
       },
       instances: 1,
