8b8f16176d76b6fa868ced8001019f638ec0b672
yki25 <yki25@uclive.ac.nz>
2025-08-30 16:26:44 +1200

    allowing cross origin for chatbot mocking

---
+++ phase_1/central_api/src/middleware.ts
@@ -0,0 +1,13 @@
+import type { MiddlewareHandler } from 'hono';
+
+export const corsMiddleware: MiddlewareHandler = async (c, next) => {
+    c.header('Access-Control-Allow-Origin', '*');
+    c.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    c.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (c.req.method === 'OPTIONS') {
+        return new Response('', { status: 204 });
+    }
+
+    await next();
+};
